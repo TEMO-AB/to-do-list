23e35b4920da26d16c3aa935d7c649bc7729ee57
temo <tamimabotafish@gmail.com>
2020-01-21 12:04:32 +0100

    installing icons

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Weather from './Component/weather';
+import 'bootstrap/dist/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+
+    </div>
+  );
+}
+
+export default App;
