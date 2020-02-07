e893d147276e837e03ad9aca12f625e41c67d24f
temo <tamimabotafish@gmail.com>
2020-02-07 13:15:29 +0100

    adding new Component

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import Todo from './Component/todo';
-import './App.css';
+import React, { Component } from 'react';
+import Todo from './Component/todos';
 
-function App() {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some stuff' },
+      { id: 2, content: 'play fefa' }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todo todo={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
